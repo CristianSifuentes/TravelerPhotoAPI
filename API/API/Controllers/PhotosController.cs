f1603dac6f93ce5ec44fca7f892b70bbfbc22f49
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-02-10 23:17:09 -0600

    10022020: Se agregan metodos de post y get, para crear y editar fotografias

--- API/API/Controllers/PhotosController.cs
+++ API/API/Controllers/PhotosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dto;
+using API.Models;
 using API.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -40,6 +41,46 @@ namespace API.Controllers
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
+        }
+
+        [HttpGet("{photoId}")]
+        public async Task<ActionResult<PhotosDto>> Get(int photoId)
+        {
+            try
+            {
+                var result = await _eventRepository.GetTrip(photoId);
+
+                if (result == null) return NotFound();
+
+                var mappedEntity = _mapper.Map<PhotosDto>(result);
+                return Ok(mappedEntity);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PhotosDto>> Post(PhotosDto dto)
+        {
+            try
+            {
+                var mappedEntity = _mapper.Map<Photos>(dto);
+                _eventRepository.Add(mappedEntity);
+
+                if (await _eventRepository.Save())
+                {
+                    var location = _linkGenerator.GetPathByAction("Get", "Photos", new { mappedEntity.Id });
+                    return Created(location, _mapper.Map<PhotosDto>(mappedEntity));
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException.Message);
+            }
+
+            return BadRequest();
         }
 
     }
